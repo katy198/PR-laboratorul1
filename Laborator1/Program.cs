679b680b703ed06bf0b39eddd32f558b32019562
Ecaterina Pavlenco <katy.pey@mail.ru>
2020-04-22 20:05:00 +0300

    first commit

---
+++ Laborator1/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Laborator1
+{
+    class Program
+    {
+        private static Semaphore _pool;
+        private static int _padding;
+        public static bool ValidateServerCertificate(
+            object sender, X509Certificate certificate,
+            X509Chain chain, SslPolicyErrors policyErrors)
+        {
+            if (policyErrors == SslPolicyErrors.None)
+                return true;
+            Console.WriteLine("Certificate error: {0}", policyErrors);
+            return false;
+        }
+        static void Main(string[] args)
+        {
+
+            _pool = new Semaphore(4, 4);
+            TcpClient client = new TcpClient("81.180.74.23", 443);
+
+            SslStream ssl = new SslStream(
+                client.GetStream(),
+                false,
+                new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                null
+                )
+                ;
+
+            try
+            {
+                ssl.AuthenticateAsClient("utm.md");
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine("Authentication failed - closing the connection.");
+                client.Close();
+                return;
+            }
+
+            byte[] buffer = new byte[2048];
+            
+            string request_url = "GET / HTTP/1.1\r\n" +"Host: utm.md\r\n\r\n"+"Accept: *\r\n\r\n";
+
+
+            byte[] request = Encoding.UTF8.GetBytes(String.Format(request_url));
+            ssl.Write(request, 0, request.Length);
+
+            ssl.Flush();
+
+            int bytes = -1;
+            int i=-1;
+            StringBuilder messageData = new StringBuilder();
+            do
+            {
+                bytes =ssl.Read(buffer, 0, buffer.Length);
+
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, buffer.Length)];
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                messageData.Append(chars);
+                if (messageData.ToString().IndexOf("<EOF>") != -1)
+                {
+                    break;
+                }
+            } while (bytes!=0);
+            Console.WriteLine(messageData.ToString());
+
+            string response = messageData.ToString();
+            Regex regex = new Regex(@"wp-content/(.*?)(?:jpg|gif|png)");
+            var mach = regex.Matches(response);
+
+            var list = new List<string>();
+            foreach (Match m in mach)
+            {
+                var path = m.Value;
+                list.Add(m.Value);
+                Console.WriteLine(m.Value);
+            }
+            ssl.Close();
+
+            for (int index = 0; index <=51; index++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(SaveImage));
+                Console.WriteLine(index);
+                t.Start(list[index]);
+                
+            }
+            Console.ReadKey();
+
+        }
+
+
+        public static void SaveImage(object name_path)
+        {
+            TcpClient client = new TcpClient("81.180.74.23", 443);
+            
+
+            SslStream ssl = new SslStream(
+                client.GetStream(),
+                false,
+                new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                null
+                );
+
+            try
+            {
+                ssl.AuthenticateAsClient("utm.md");
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine("Authentication failed - closing the connection.");
+                client.Close();
+                return;
+            }
+
+            Console.WriteLine("Image Path: {0} - INITIALIZED  ", (string)name_path);
+            _pool.WaitOne();
+            string data = (string)name_path;
+            string data1=data.Replace(@"\\", "");
+            string data2 = data1.Replace(@"/", "");
+            string data3 = data2.Replace(@"\", "");
+
+
+
+            int padding = Interlocked.Add(ref _padding, 100);
+
+            var saveLocation = $@"E:\img\{ padding.ToString()+data3}";
+            
+
+            byte[] imageBytes;
+
+            byte[] buffer = new byte[2048];
+
+            var request_img = "GET https://utm.md/" + (string)name_path + " HTTP/1.1\r\n" +
+            "Host: utm.md\r\n" +
+            "Content-Lenght: 0 \r\n"
+            + "\r\n";
+
+            byte[] message = Encoding.UTF8.GetBytes(request_img);
+            ssl.Write(message, 0, message.Length);
+            ssl.Flush();
+
+            int bytes = -1;
+            int i = 0;
+            StringBuilder messageData = new StringBuilder();
+            var resp = new List<byte>();
+            do
+            {
+                bytes = ssl.Read(buffer, 0, buffer.Length);
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, buffer.Length)];
+
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                if (i >= 1)
+                {
+
+                    resp.AddRange(buffer);
+                    messageData.Append(chars);
+                }
+                i++;
+                if (messageData.ToString().IndexOf("<EOF>") != -1)
+                {
+                    break;
+                }
+
+            } while (bytes != 0);
+
+
+            var img = resp.ToArray();
+            Bitmap bmp;
+            using (var ms = new MemoryStream(img))
+            {
+                bmp = new Bitmap(ms);
+                bmp.Save(saveLocation);
+            }
+            Console.WriteLine("Image Path : {0} - UNLOADED ", (string)name_path, _pool.Release()); 
+        }
+    }
+}
